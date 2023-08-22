78924f58e377ee906d5b2eb224359b5f9475760f
Grigorii Belov <belovgrigory139@gmail.com>
2023-08-22 20:49:45 +0300

    modified:   src/components/PersonsList.js

--- src/components/PersonsList.js
+++ src/components/PersonsList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person';
 
 function PersonsList() {
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
     const persons = [
         {
             id: 1,
@@ -23,11 +24,19 @@ function PersonsList() {
         },
     ];
     const personList = persons.map(x => <Person key={x.name} person={x} />)
+    const nameList = names.map((name, anyindex) => <h3 key={anyindex}>{anyindex + 1}. {name}</h3>)
     //key prop SHOULD BE UNIQUE, but it isn't important whether 
     //it's ID or NAME or whatever as long as it's unique
 
   return (
+    <>
+    {/* WE NEED an "enclosing tag", hence <>all the stuff</>, 
+    because return cannot work if there are multiple tags in it, 
+    only with one "master tag"   */}
     <div>{personList}</div>
+    <div>Alternatively,</div>
+    <div>{nameList}</div>
+    </>
   )
 }
 
