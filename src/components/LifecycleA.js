df04b2bb6cad303b3ef8df9cd6ad055ef37a3a85
Grigorii Belov <belovgrigory139@gmail.com>
2023-08-30 23:27:39 +0300

    modified:   src/components/LifecycleA.js 	modified:   src/components/LifecycleB.js

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -24,6 +24,29 @@ class LifecycleA extends Component {
     }
 
 
+    shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+      this.setState({
+        name: 'Codevolution'
+      })
+    }
+
+
   render() {
     console.log('Lifecycle A render')
     return (
@@ -32,6 +55,10 @@ class LifecycleA extends Component {
         <div>
             Lifecycle A
         </div>
+
+        <button onClick={this.changeState}>
+            Change State
+        </button>
 
         <LifecycleB />
 
