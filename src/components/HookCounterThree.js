1a16189a6e2563ecfe9317eabb2ba8589b91326c
Grigorii Belov <belovgrigory139@gmail.com>
2023-11-18 00:01:00 +0300

    new file:   src/components/HookCounterThree.js

---
+++ src/components/HookCounterThree.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function HookCounterThree() {
+
+    const [name, setName] = useState({
+        firstName: '',
+        lastName: ''
+    })
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HookCounterThree
