b9066b66dfaa800bed5e1cb4f3b503ed76f4fbed
Grigorii Belov <belovgrigory139@gmail.com>
2023-11-23 23:11:27 +0300

    modified:   src/App.js 	new file:   src/components/HookCounterFour.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,8 @@ import PostForm from './components/PostForm';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/HookCounterThree'
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 // function App() {
 //   return (
@@ -62,7 +63,9 @@ class App extends Component {
 
         {/* <HookCounterTwo /> */}
 
-        <HookCounterThree />
+        {/* <HookCounterThree /> */}
+
+        <HookCounterFour />
 
         {/* <PostForm /> */}
 
