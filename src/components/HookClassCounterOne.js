294622ce8c24c72f078246f48636443e59c7502a
Grigorii Belov <belovgrigory139@gmail.com>
2023-11-25 23:56:37 +0300

    modified:   src/components/HookClassCounterOne.js 	modified:   src/components/HookFunctionCounterOne.js

--- src/components/HookClassCounterOne.js
+++ src/components/HookClassCounterOne.js
@@ -6,7 +6,8 @@ class HookClassCounterOne extends Component {
     super(props)
   
     this.state = {
-       count: 0
+       count: 0,
+       name: ''
     }
   }
 
@@ -15,12 +16,22 @@ class HookClassCounterOne extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    if(prevState.count !== this.state.count) {
+    console.log('Updating document title')
     document.title = `Clicked ${this.state.count} times`
-  }
+  }}
   
   render() {
     return (
       <div>
+        <input 
+          type='text' 
+          value={this.state.name} 
+          onChange={e => {
+            this.setState({ name: e.target.value })
+            }}
+        ></input>
+
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
             Class Clicked {this.state.count} times
         </button>
