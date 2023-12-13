23e862460c7f2849a0da6e3f1377295f5389c796
Grigorii Belov <belovgrigory139@gmail.com>
2023-12-13 12:08:42 +0300

    modified:   src/App.js 	new file:   src/components/FunctionC.js 	new file:   src/components/FunctionE.js 	new file:   src/components/FunctionF.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Mashehu from './components/Mashehu';
-import Countergpt from './components/Countergpt';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import PersonsList from './components/PersonsList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Mashehu from './components/Mashehu';
+// import Countergpt from './components/Countergpt';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
+// import PersonsList from './components/PersonsList';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
 import './appStyles.css';
-import styles from './appStyles.module.css';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
-import Hero from './components/Hero';
-import ErrorBoundary from './components/ErrorBoundary';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
-import Counter37 from './components/Counter37';
-import ComponentC from './components/ComponentC';
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterFour from './components/HookCounterFour';
-import HookClassCounterOne from './components/HookClassCounterOne';
-import HookFunctionCounterOne from './components/HookFunctionCounterOne';
-import HookClassMouse from './components/HookClassMouse';
-import HookFunctionMouse from './components/HookFunctionMouse';
-import HookMouseContainer from './components/HookMouseContainer';
-import HookIntervalClassCounter from './components/HookIntervalClassCounter';
-import HookIntervalFunctionCounter from './components/HookIntervalFunctionCounter';
-import DataFetching from './components/DataFetching';
+// import styles from './appStyles.module.css';
+// import Form from './components/Form';
+// import LifecycleA from './components/LifecycleA';
+// import FragmentDemo from './components/FragmentDemo';
+// import Table from './components/Table';
+// import ParentComp from './components/ParentComp';
+// import RefsDemo from './components/RefsDemo';
+// import FocusInput from './components/FocusInput';
+// import FRParentInput from './components/FRParentInput';
+// import PortalDemo from './components/PortalDemo';
+// import Hero from './components/Hero';
+// import ErrorBoundary from './components/ErrorBoundary';
+// import ClickCounter from './components/ClickCounter';
+// import HoverCounter from './components/HoverCounter';
+// import ClickCounterTwo from './components/ClickCounterTwo';
+// import HoverCounterTwo from './components/HoverCounterTwo';
+// import User from './components/User';
+// import Counter37 from './components/Counter37';
+// import ComponentC from './components/ComponentC';
+// import PostList from './components/PostList';
+// import PostForm from './components/PostForm';
+// import ClassCounter from './components/ClassCounter';
+// import HookCounter from './components/HookCounter';
+// import HookCounterTwo from './components/HookCounterTwo';
+// import HookCounterThree from './components/HookCounterThree';
+// import HookCounterFour from './components/HookCounterFour';
+// import HookClassCounterOne from './components/HookClassCounterOne';
+// import HookFunctionCounterOne from './components/HookFunctionCounterOne';
+// import HookClassMouse from './components/HookClassMouse';
+// import HookFunctionMouse from './components/HookFunctionMouse';
+// import HookMouseContainer from './components/HookMouseContainer';
+// import HookIntervalClassCounter from './components/HookIntervalClassCounter';
+// import HookIntervalFunctionCounter from './components/HookIntervalFunctionCounter';
+// import DataFetching from './components/DataFetching';
+import FunctionC from './components/FunctionC';
 
 // function App() {
 //   return (
@@ -103,10 +104,12 @@ class App extends Component {
         <UserContext.Provider value={'Menkalinan'}>
 
         <ChannelContext.Provider value={'Codevolution'}>
-        <ComponentC />
+        <FunctionC />
         </ChannelContext.Provider>
 
         </UserContext.Provider>
+
+        {/* NEXT: useReducer Hook! */}
 
         {/* <User render={(isLoggedIn) => isLoggedIn ? 'Menkalinan' : 'Guest'}/>
 
