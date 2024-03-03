a891f5a338081bbda0d00610aa53b27f21da0b11
Grigorii Belov <belovgrigory139@gmail.com>
2024-03-04 00:01:33 +0300

    `	modified:   src/components/AXHookCounterThreeX.js

--- src/components/AXHookCounterThreeX.js
+++ src/components/AXHookCounterThreeX.js
@@ -12,6 +12,16 @@ function AXHookCounterThreeX() {
         onChange={e => setName({ ...name, firstName: e.target.value})}
       />
 
+      <input
+        type="text"
+        value={name.lastName}
+        onChange={e => setName({ ...name, lastName: e.target.value})}
+      />
+
+      <input type="text"/>
+      <h2>Your first name is {name.firstName}</h2>
+      <h2>Your last name is {name.lastName}</h2>
+      <h2>{JSON.stringify(name)}</h2>
     </form>
   )
 }
