127e1f2a209de0482e5f236ec51a83c6233dbfc8
Grigorii Belov <belovgrigory139@gmail.com>
2024-03-19 22:30:46 +0300

    modified:   src/App.js 	modified:   src/components/AXClassCounterOne.js 	modified:   src/components/AXHookCounterOne.js

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
   return (
     <div className='App'>
 
-      <AXHookCounterOneX />
+      <AXHookCounterOne />
 
     </div>
   );
--- src/components/AXClassCounterOne.js
+++ src/components/AXClassCounterOne.js
@@ -6,7 +6,8 @@ class AXClassCounterOne extends Component {
     super(props)
   
     this.state = {
-       count: 0
+       count: 0,
+       name: ''
     }
   }
 
@@ -15,12 +16,23 @@ class AXClassCounterOne extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    if(prevState.count !== this.state.count) {
+    console.log('Updating document title')
     document.title = `Clicked ${this.state.count} times`
+  }
   }
   
   render() {
     return (
       <div>
+        <input 
+          type="text" 
+          value={this.state.name} 
+          onChange={e => {
+            this.setState({ name: e.target.value })
+          }}
+        />
+
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
             Click {this.state.count} times
         </button>
